907c3c487ea46794a2ac42f85cf1711f4c96aec6
Junaxer <juniordelarocha15@hotmail.com>
2025-06-06 08:04:28 -0600

    Cambio de condicion

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ def main():
     numeros = [30, 20, 8, 40, 50]
     objetivo = 30
     resultado = buscar_numero(numeros, objetivo)  
-    if resultado != -1:
+    if resultado:
         print("Número encontrado en el índice", resultado)  
     else:
         print("Número no encontrado")
