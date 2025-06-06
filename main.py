1b5f4afcd75e16e18edee637721a030d8dceaeb6
Junaxer <juniordelarocha15@hotmail.com>
2025-06-06 07:55:48 -0600

    Presentacion-debug

---
+++ main.py
@@ -0,0 +1,16 @@
+def buscar_numero(lista, objetivo):
+    for i in range(len(lista)):
+        if lista[i] == objetivo:  
+            return i  
+    return -1
+
+def main():
+    numeros = [30, 20, 8, 40, 50]
+    objetivo = 30
+    resultado = buscar_numero(numeros, objetivo)  
+    if resultado != -1:
+        print("Número encontrado en el índice", resultado)  
+    else:
+        print("Número no encontrado")
+
+main()
